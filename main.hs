d86a19455fab7493071842a6422f40c675f0c17e
Breckin Loggins <bloggins@greatfoundry.com>
2013-07-10 22:47:48 -0700

    chapter_01

---
+++ main.hs
@@ -0,0 +1,11 @@
+-- Write yourself a scheme in 48 hours
+-- http://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours
+
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do
+	args <- getArgs
+	putStrLn ("Hello, " ++ args !! 0)
+
