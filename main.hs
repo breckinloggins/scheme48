3181b0afc36d570f7d7457a5ea405148c3113d6b
Breckin Loggins <bloggins@greatfoundry.com>
2013-07-13 12:25:45 -0700

    chapter_02

--- main.hs
+++ main.hs
@@ -2,12 +2,57 @@
 -- http://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours
 
 module Main where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+	| List [LispVal]
+	| DottedList [LispVal] LispVal
+	| Number Integer
+	| String String
+	| Bool Bool
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseString :: Parser LispVal
+parseString = do
+	char '"'
+	x <- many (noneOf "\"")
+	char '"'
+	return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+	first <- letter <|> symbol
+	rest <- many (letter <|> digit <|> symbol)
+	let atom = first:rest
+	return $ case atom of
+		"#t" -> Bool True
+		"#f" -> Bool False
+		_ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+	<|> parseString
+	<|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+	Left err -> "No match: " ++ show err
+	Right val -> "Found value"
 
 main :: IO ()
 main = do
-	putStrLn "Please type your name"
-	name <- getLine
 	args <- getArgs
-	putStrLn (name ++ ", sum is: " ++ show (sum (map read args :: [Int])))
-
+	putStrLn (case length args of
+		0 -> "no input"
+		_ -> (readExpr (args !! 0)))
